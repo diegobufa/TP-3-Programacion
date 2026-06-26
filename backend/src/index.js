d3c27d1167e1e0cb40ab9945678b0db491331d82
unknown <LeonardoRamos503@gmail.com>
2026-06-26 16:50:31 -0300

    Agregado usuarios de test a la Base de Datos

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,13 @@ import express from "express";
 import cors from "cors";
 import { PORT } from "./config.js";
 import { sequelize } from "./db.js";
+import bcrypt from "bcrypt";
 
 import authRoutes from "../routes/auth.routes.js";
 import productRoutes from "../routes/product.routes.js";
 
 import { Rol } from "../models/Rol.js";
-import "../models/Usuario.js";
+import { Usuario } from "../models/Usuario.js";
 import "../models/Pedido.js";
 import "../models/Producto.js";
 import "../models/DetallePedido.js";
@@ -18,6 +19,9 @@ app.use(cors());
 app.use(express.json());
 app.use(authRoutes);
 app.use(productRoutes);
+
+const passwordAdmin = await bcrypt.hash("admin123", 10);
+
 
 try {
   await sequelize.sync();
@@ -37,7 +41,39 @@ try {
             defaults: { nombre: "SuperAdmin" }
         });
 
+  await Usuario.findOrCreate({
+        where: { email: "admin@admin.com" },
+        defaults: {
+        nombre: "Admin",
+        apellido: "Administrador",
+        usuario: "Admin",
+        telefono: "3465000000",
+        email: "admin@admin.com",
+        password: passwordAdmin,
+        provincia: "Santa Fe",
+        localidad: "Firmat",
+        calle: "San Martin",
+        altura: 123,
+        fk_rol: 2
+      }
+    });
 
+await Usuario.findOrCreate({
+  where: { email: "superadmin@admin.com" },
+  defaults: {
+    nombre: "SuperAdmin",
+    apellido: "SuperAdmin",
+    usuario: "superadmin",
+    telefono: "3465000001",
+    email: "superadmin@admin.com",
+    password: passwordAdmin,
+    provincia: "Santa Fe",
+    localidad: "Firmat",
+    calle: "Belgrano",
+    altura: 456,
+    fk_rol: 3
+  }
+});
   app.listen(PORT, () => {
     console.log(`Servidor escuchando en puerto ${PORT}`);
   });
