90f8541e8486021c4ae16d51935ed40b9f6e5f7e
unknown <LeonardoRamos503@gmail.com>
2026-06-26 01:07:44 -0300

    Agregado Servicio de Autorizacion

---
+++ frontend/src/services/authServices.js
@@ -0,0 +1,63 @@
+import { API_URL } from "../constants/productConstants";
+
+export const authservices = {
+  
+  registro: async (datosUsuario) => {
+    const res = await fetch(`${API_URL}/auth/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(datosUsuario) 
+    });
+    
+    if (!res.ok) {
+      const errorData = await res.json().catch(() => ({}));
+      throw new Error(errorData.message || "Error al registrar usuario");
+    }
+    return res.json();
+  },
+
+  login: async (email, password) => {
+    const res = await fetch(`${API_URL}/auth/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password })
+    });
+
+    if (!res.ok) {
+      const errorData = await res.json().catch(() => ({}));
+      throw new Error(errorData.message || "Credenciales incorrectas");
+    }
+
+    const token = await res.text();
+    
+    return { token, email }; 
+  },
+
+  getUsuarios: async (token) => {
+    const res = await fetch(`${API_URL}/usuarios`, {
+      headers: { "Authorization": `Bearer ${token}` }
+    });
+    if (!res.ok) throw new Error("No autorizado");
+    return res.json();
+  },
+
+  actualizarRol: async (id, fk_rol, token) => {
+    const res = await fetch(`${API_URL}/usuarios/${id}`, {
+      method: "PUT",
+      headers: { 
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      },
+      body: JSON.stringify({ fk_rol })
+    });
+    return res.json();
+  },
+
+  eliminarUsuario: async (id, token) => {
+    const res = await fetch(`${API_URL}/usuarios/${id}`, {
+      method: "DELETE",
+      headers: { "Authorization": `Bearer ${token}` }
+    });
+    if (!res.ok) throw new Error("No se pudo eliminar");
+  }
+};
